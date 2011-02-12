2a8bbf8bd48a280ad521e4caad9406ac64220d73
Olivier Sobrie <olivier@sobrie.be>
2011-02-12 16:37:18 +0100

    Put global utils function in etri_utils.py

--- etri-bm-global-compat.py
+++ etri-bm-global-compat.py
@@ -3,6 +3,7 @@ import random_data
 import debug
 import glpk
 import etri
+from etri_utils import *
 from optparse import OptionParser
 
 default_seed = 1
@@ -10,34 +11,6 @@ default_nalternatives = 1000
 default_ncriteria = 5
 default_nprofiles = 1
 default_nlearning = 100
-
-def create_model(nalternatives, ncriteria, nprofiles):
-    alternatives = [ "a%d" % (i+1) for i in range(nalternatives) ]
-    criteria = [ "g%d" % (i+1) for i in range(ncriteria) ]
-    palternatives = [ "b%d" % (i+1) for i in range(nprofiles) ]
-    return (alternatives, criteria, palternatives)
-
-def generate_random_data(seed, alternatives, criteria, palternatives):
-    random_data.set_seed(seed)
-    profiles = random_data.generate_random_profiles(palternatives, criteria)
-    weights = random_data.generate_random_weights(criteria)
-    lbda = random_data.generate_random_lambda() 
-    pt = random_data.generate_random_pt(alternatives, criteria)
-    return (pt, profiles, weights, lbda)
-
-def etri_infer_parameters(nlearning, criteria, pt, affectations, nprofiles):
-    learning_alts = [ "a%d" % (i+1) for i in range(nlearning) ]
-    categories = [ (i+1) for i in range(nprofiles+1) ]
-    categories_rank = {}
-    for i, cat in enumerate(categories):
-        categories_rank[cat] = i+1
-    infile = glpk.create_input_file(learning_alts, criteria, pt, categories, categories_rank, affectations) 
-    print "GLPK file:", infile.name
-    (status, output) = glpk.solve(infile.name, "models/etri_bm_global_compat.mod")
-    if status:
-        sys.exit("gklp returned status %d" % status)
-    infile.close()
-    return glpk.parse_output(output, learning_alts, criteria)
 
 def parse_cmdline(argv=None):
     parser = OptionParser()
@@ -86,7 +59,7 @@ def main(argv=None):
     affectations = model.pessimist() 
 
     # Infer ELECTRE Tri parameters
-    (iweights, iprofiles, ilbda, icompat, info) = etri_infer_parameters(nlearning, criteria, pt, affectations, nprofiles)
+    (iweights, iprofiles, ilbda, icompat, info) = etri_infer_parameters(nlearning, criteria, pt, affectations, nprofiles, "models/etri_bm_global_compat.mod")
 
     # Apply ELECTRE Tri model with infered parameters 
     modeli = etri.electre_tri(pt, iprofiles, iweights, ilbda) 
---
+++ etri_utils.py
@@ -0,0 +1,32 @@
+import sys
+import random_data
+import debug
+import glpk
+
+def create_model(nalternatives, ncriteria, nprofiles):
+    alternatives = [ "a%d" % (i+1) for i in range(nalternatives) ]
+    criteria = [ "g%d" % (i+1) for i in range(ncriteria) ]
+    palternatives = [ "b%d" % (i+1) for i in range(nprofiles) ]
+    return (alternatives, criteria, palternatives)
+
+def generate_random_data(seed, alternatives, criteria, palternatives):
+    random_data.set_seed(seed)
+    profiles = random_data.generate_random_profiles(palternatives, criteria)
+    weights = random_data.generate_random_weights(criteria)
+    lbda = random_data.generate_random_lambda() 
+    pt = random_data.generate_random_pt(alternatives, criteria)
+    return (pt, profiles, weights, lbda)
+
+def etri_infer_parameters(nlearning, criteria, pt, affectations, nprofiles, model):
+    learning_alts = [ "a%d" % (i+1) for i in range(nlearning) ]
+    categories = [ (i+1) for i in range(nprofiles+1) ]
+    categories_rank = {}
+    for i, cat in enumerate(categories):
+        categories_rank[cat] = i+1
+    infile = glpk.create_input_file(learning_alts, criteria, pt, categories, categories_rank, affectations) 
+    print "GLPK file:", infile.name
+    (status, output) = glpk.solve(infile.name, model)
+    if status:
+        sys.exit("gklp returned status %d" % status)
+    infile.close()
+    return glpk.parse_output(output, learning_alts, criteria)
