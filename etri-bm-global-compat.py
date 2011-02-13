6d7f3338c840db53521d0cb0ed28e27d2324fa90
Olivier Sobrie <olivier@sobrie.be>
2011-02-13 22:49:11 +0100

    [BM-global-compat] Add option to set errors in affectations of learning alts

--- etri-bm-global-compat.py
+++ etri-bm-global-compat.py
@@ -1,4 +1,5 @@
 import sys
+import random
 import random_data
 import debug
 import glpk
@@ -11,6 +12,7 @@ default_nalternatives = 1000
 default_ncriteria = 5
 default_nprofiles = 1
 default_nlearning = 100
+default_error = 0
 
 def parse_cmdline(argv=None):
     parser = OptionParser()
@@ -19,6 +21,7 @@ def parse_cmdline(argv=None):
     parser.add_option("-p", "--nprofiles", dest="nprofiles")
     parser.add_option("-r", "--nlearning", dest="nlearning")
     parser.add_option("-s", "--seed", dest="seed")
+    parser.add_option("-e", "--error", dest="error")
     (options, args) = parser.parse_args(argv[1:])
     nalternatives = int(options.nalternatives)
     if not nalternatives:
@@ -35,15 +38,35 @@ def parse_cmdline(argv=None):
     seed = int(options.seed)
     if not seed:
         seed = default_seed
+    error = int(options.error)
+    if not error:
+        error = default_error
 
-    return (nalternatives, ncriteria, nprofiles, nlearning, seed)
+    return (nalternatives, ncriteria, nprofiles, nlearning, seed, error)
+
+def add_errors_in_learning_alts(affectations, nlearning, nprofiles, errors):
+    learning_alts = [ "a%d" % (i+1) for i in range(nlearning) ]
+    ncat = nprofiles
+    for alt in learning_alts:
+        if errors == 0:
+            break
+
+        old =  affectations[alt]
+        new = old
+        while old == new:
+            new = ((old+random.randint(1, 10)) % (nprofiles+1))+1
+        print "alt", alt
+        print "old", old
+        print "new", new
+        affectations[alt] = new
+        errors = errors - 1
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
 
     # Parse command line
-    (nalternatives, ncriteria, nprofiles, nlearning, seed) = parse_cmdline(argv)
+    (nalternatives, ncriteria, nprofiles, nlearning, seed, error) = parse_cmdline(argv)
     print "Input parameters"
     print "================"
     print "Nalternatives:", nalternatives
@@ -57,6 +80,9 @@ def main(argv=None):
     (pt, profiles, weights, lbda) = generate_random_data(seed, alternatives, criteria, palternatives)
     model = etri.electre_tri(pt, profiles, weights, lbda) 
     affectations = model.pessimist() 
+
+    # Add errors in learning alternatives
+    add_errors_in_learning_alts(affectations, nlearning, nprofiles, error)
 
     # Infer ELECTRE Tri parameters
     (iweights, iprofiles, ilbda, icompat, info) = etri_infer_parameters(nlearning, criteria, pt, affectations, nprofiles, "models/etri_bm_global_compat.mod")
