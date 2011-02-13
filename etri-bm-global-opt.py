41b98b5a94f84ef3df6c1332d81548ce3eda5319
Olivier Sobrie <olivier@sobrie.be>
2011-02-13 22:52:35 +0100

    [BM-global-opt] Add another inference procedure test

---
+++ etri-bm-global-opt.py
@@ -0,0 +1,79 @@
+import sys
+import random_data
+import debug
+import glpk
+import etri
+from etri_utils import *
+from optparse import OptionParser
+
+default_seed = 1
+default_nalternatives = 1000
+default_ncriteria = 5
+default_nprofiles = 1
+default_nlearning = 100
+
+def parse_cmdline(argv=None):
+    parser = OptionParser()
+    parser.add_option("-a", "--nalternatives", dest="nalternatives")
+    parser.add_option("-c", "--ncriteria", dest="ncriteria")
+    parser.add_option("-p", "--nprofiles", dest="nprofiles")
+    parser.add_option("-r", "--nlearning", dest="nlearning")
+    parser.add_option("-s", "--seed", dest="seed")
+    (options, args) = parser.parse_args(argv[1:])
+    nalternatives = int(options.nalternatives)
+    if not nalternatives:
+        nalternatives = default_nalternatives
+    ncriteria = int(options.ncriteria)
+    if not ncriteria:
+        ncriteria = default_ncriteria
+    nprofiles = int(options.nprofiles)
+    if not nprofiles:
+        nprofiles = default_nprofiles
+    nlearning = int(options.nlearning)
+    if not nlearning:
+        nlearning = default_nlearning
+    seed = int(options.seed)
+    if not seed:
+        seed = default_seed
+
+    return (nalternatives, ncriteria, nprofiles, nlearning, seed)
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    # Parse command line
+    (nalternatives, ncriteria, nprofiles, nlearning, seed) = parse_cmdline(argv)
+    print "Input parameters"
+    print "================"
+    print "Nalternatives:", nalternatives
+    print "Ncriteria:", ncriteria
+    print "Nprofiles:", nprofiles
+    print "Nlearning:", nlearning
+    print "Seed:", seed
+
+    # Create a model
+    (alternatives, criteria, palternatives) = create_model(nalternatives, ncriteria, nprofiles)
+    (pt, profiles, weights, lbda) = generate_random_data(seed, alternatives, criteria, palternatives)
+    model = etri.electre_tri(pt, profiles, weights, lbda) 
+    affectations = model.pessimist() 
+
+    # Infer ELECTRE Tri parameters
+    (iweights, iprofiles, ilbda, icompat, info) = etri_infer_parameters(nlearning, criteria, pt, affectations, nprofiles, "models/etri_bm_global_opt.mod")
+
+    # Apply ELECTRE Tri model with infered parameters 
+    modeli = etri.electre_tri(pt, iprofiles, iweights, ilbda) 
+    iaffectations = modeli.pessimist()
+
+    # Print result
+    print "Output"
+    print "======"
+    print "Time used:", info[0]
+    print "Memory used:", info[1]
+    debug.print_lambda(lbda, ilbda)
+    debug.print_weights(weights, criteria, iweights)
+    debug.print_profiles(profiles, criteria, iprofiles)
+    debug.print_performance_table_with_assignements(pt, alternatives, criteria, affectations, iaffectations, icompat)
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ models/etri_bm_global_opt.mod
@@ -0,0 +1,105 @@
+/* Global inference of ELECTRE TRI Bouyssou-Marchant model parameters */
+
+/* Parameters */
+param ncat, integer, >= 2; /* number of categories */ 
+param nalt, integer, > 0; /* number of alternatives */
+param ncrit, integer, > 0; /* number of criteria */
+
+set ALTS := 1..nalt; /* set of alternatives */
+set CRIT := 1..ncrit; /* set of criteria */
+
+param perfs{i in ALTS, j in CRIT};
+param assign{ALTS};
+
+/* Variables */
+var epsilon = 10e-5;
+var lambda  >= 0.5;
+var c_sup{ALTS,CRIT}  >= 0;
+var c_inf{ALTS,CRIT}  >= 0;
+var weight{CRIT}  >= 0;
+var d_sup{ALTS,CRIT}, binary;
+var d_inf{ALTS,CRIT}, binary;
+var gb{0..ncat,CRIT}  >= 0;
+var x{ALTS};
+var y{ALTS};
+var alpha;
+
+/* Objective */
+maximize obj: alpha;
+
+/* Constraints */
+s.t. scinf{i in ALTS}:
+	sum{j in CRIT} c_inf[i,j] = lambda + y[i];
+
+s.t. scsup{i in ALTS}:
+	sum{j in CRIT} c_sup[i,j] + x[i] = lambda;
+
+s.t. alphax{i in ALTS}:
+	alpha <= x[i];
+
+s.t. alphay{i in ALTS}:
+	alpha <= y[i];
+
+s.t. cinf2{i in ALTS, j in CRIT}:
+	c_inf[i,j] <= d_inf[i,j];
+
+s.t. csup2{i in ALTS, j in CRIT}:
+	c_sup[i,j] <= d_sup[i,j];
+
+s.t. cinf3{i in ALTS, j in CRIT}:
+	c_inf[i,j] >= d_inf[i,j] - 1 + weight[j];
+
+s.t. csup3{i in ALTS, j in CRIT}:
+	c_sup[i,j] >= d_sup[i,j] - 1 + weight[j];
+
+s.t. dinf{i in ALTS, j in CRIT}:
+	d_inf[i,j] >= perfs[i,j] - gb[assign[i]-1,j] + epsilon;
+
+s.t. dsup{i in ALTS, j in CRIT}:
+	d_sup[i,j] >= perfs[i,j] - gb[assign[i],j] + epsilon;
+
+s.t. dinf2{i in ALTS, j in CRIT}:
+	d_inf[i,j] <= perfs[i,j] - gb[assign[i]-1,j] + 1;
+
+s.t. dsup2{i in ALTS, j in CRIT}:
+	d_sup[i,j] <= perfs[i,j] - gb[assign[i],j] + 1;
+
+s.t. gblim{i in 1..ncat-1, j in CRIT}:
+	gb[i,j] <= gb[i+1,j];
+
+s.t. gbliminf{j in CRIT}:
+	gb[0,j] = 0 + epsilon;
+
+s.t. gblimsup{j in CRIT}:
+	gb[ncat,j] = 1;
+
+s.t. wmin{j in CRIT}:
+	weight[j] <= 1;
+
+wsum: sum{j in CRIT} weight[j] = 1;
+lbdamin: lambda >= 0.5;
+lbdamax: lambda <= 1;
+
+solve;
+
+printf "SOLUTION:\n";
+
+printf "### Lambda ###\n";
+printf lambda;
+printf "\n### Lambda ###\n";
+
+printf "### Profiles ###\n";
+for {i in 1..ncat-1}
+{
+	for {j in CRIT}	
+		printf "%g\t", gb[i,j];
+	printf "\n";
+}
+printf "### Profiles ###\n";
+
+printf "### Criteria weights ###\n";
+for {j in CRIT}
+	printf "%g\t", weight[j];
+printf "\n### Criteria weights ###\n";
+
+end;
